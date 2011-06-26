f4f3b482810594f47ee720d76ef526a119071757
Brian Dewey <bdewey@gmail.com>
2011-06-25 22:59:28 -0700

    Initial checkin of DropVaultKeys. If all goes well, the actual keys are smudged out.

---
+++ Classes/DropVaultKeys.h
@@ -0,0 +1,38 @@
+//
+//  DropVaultKeys.h
+//
+//  Contains the API keys needed to work with DropBox.
+//
+//  Created by Brian Dewey on 6/25/11.
+//  Copyright 2011 Brian Dewey. 
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#ifndef DropVault_DropVaultKeys_h
+#define DropVault_DropVaultKeys_h
+
+//
+//  This is the consumer key & secret for interacting with DropBox.
+//  Note that I've configured GIT so these are stripped out of the source
+//  repository. You need to get your OWN keys.
+//
+
+//#define PHOLIO_CONSUMER_KEY ...
+//#define PHOLIO_API_SECRET ...
+
+#ifndef DROPVAULT_CONSUMER_KEY
+#error You must get a consumer key and secret from http://www.dropbox.compliance
+#endif
+
+#endif
